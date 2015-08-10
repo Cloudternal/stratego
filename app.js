8380ecf9828c8544131a45c6de7616877a03e38d
tcrognon <tcrognon@gmail.com>
2015-08-10 17:18:25 -0500

    learning

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var app = express();
-
 var path = require('path');
+
+//
+// configuration
+//
 
 app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views')); 
+app.locals.pretty = true;
+app.locals.doctype = 'html';
+app.locals.basedir = app.get('views');
+
+//
+// run
+//
 
 app.listen(process.env.PORT);
+
+//
+// routes
+//
 
 app.get('/', function (req, res) {
     app.render('home', function (err, html) {
