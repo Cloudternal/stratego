2d58f0cf28c6dfa4599fe9234b3f8ef6161ce0fc
dsatfire <dsatfire@gmail.com>
2015-08-26 08:47:18 -0500

    learning

--- public/js/stratego.js
+++ public/js/stratego.js
@@ -1,31 +1,58 @@
 $(function () {
     $('button.square').on('click', function () {
-      alert("Button " + $(this).val());
+        alert("Button " + $(this).val());
+            $.ajax({
+                method: 'post',
+                url: '/squareclicked',
+                data: {
+                    square: $(this).val()
+                },
+                success: function (data) {
+                    if (!data) {
+                        alert('rendering error');
+                        return;
+                    }
+                    switch (data[0]) {
+                    case 'Success':
+                        if (data[1]) {
+                            $('.alert').html(data[1]);
+                        }
+                        break;
+                    default: break;
+                    }
+                },
+                error: function () {
+                    alert('error!');
+                },
+                complete: function () {
+                    alert('complete!');
+                }
+          });
+    });
+    $('button.startbutton').on('click', function (e) {
+        e.preventDefault();
         $.ajax({
             method: 'post',
-            url: '/squareclicked',
-            data: {
-                square: $(this).val()
-            },
+            url: '/gamestart',
+            data: $(this).serialize(),
             success: function (data) {
                 if (!data) {
                     alert('rendering error');
                     return;
                 }
                 switch (data[0]) {
-                case 'Success':
-                    if (data[1]) {
-                        $('.alert').html(data[1]);
-                    }
-                    break;
-                default: break;
+                    case 'Success':
+                        if (data[1]) {
+                            var form = $('form#play');
+                            form.before(data[1]);
+                            form.remove();                            
+                        }
+                        break;
+                    default: break;
                 }
             },
             error: function () {
                 alert('error!');
-            },
-            complete: function () {
-                alert('complete!');
             }
         });
     });
